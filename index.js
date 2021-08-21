546fae94455d48599cec5d29a5154cbcdbb6dda3
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2021-08-22 01:22:23 +0700

    config docker for mongose

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const wakeUpDyno = require('./wakeUpDyno');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/Resolves');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const pubsub = new PubSub();
 
